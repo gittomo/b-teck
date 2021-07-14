6301ac8e0dc3ea008d586fcd22d30353b30c4b0a
Tomoaki Sakai <t-sakai@genet.jp>
2021-07-14 16:52:08 +0900

    3-2 add first

--- 03/03-1.js
+++ 03/03-1.js
@@ -1,20 +1,29 @@
 const lists=[];
+function view(){
+  const commet =document.getElementById('comment');
+    commet.innerHTML = '';
+    let str = '<tr><th>ID</th><th>コメント</th><th>状態</th></tr>';
+  if(lists.length > 0){
+    lists.forEach((val, key) => {
+
+        str +=  '<tr><td>'+key+'</td><td>'+val.comment+'</td><td><button value='+key+' onclick="status(this)">'+val.status +'</button ></td><td><button value='+key+' onclick="del(this)">削除</button></td></tr>';
+    })
+  commet.innerHTML = str;
+  }
+}
 function todolist(){
     const task =document.getElementById("task");
     if(task.value !== ''){
         lists.push({'comment':task.value,'status':"作業中"});
       }
-      const commet =document.getElementById('comment');
-      commet.innerHTML = '';
-      let str = '<tr><th>ID</th><th>コメント</th><th>状態</th></tr>';
-
-      if(lists.length > 0){
-      lists.forEach((val, key) => {
-          str +=  '<tr><td>'+key+'</td><td>'+val.comment+'</td><td><button>'+val.status +'</button></td><td><button class="delete">削除</button></td></tr>';
-
-      })
-      
- 
-     }
-     commet.innerHTML = str;
+      view();
 }
+let del= (button)=>{
+  lists.splice(button.value,1);
+  view();
+}
+//  3-3 次回
+// let status= (button)=>{
+//   lists[button.value].status=(lists[button.value].status)==="作業中"?"完了":"作業中";
+//       view();
+//   }
