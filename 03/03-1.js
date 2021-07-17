7a4556900b4faa82097dcb740754d695a5b50e3e
peko-rin <who_am_i@mrj.biglobe.ne.jp>
2021-07-18 07:47:12 +0900

    3-4 modifid

--- 03/03-1.js
+++ 03/03-1.js
@@ -1,21 +1,25 @@
 const lists=[];
 let lists_new;
 const checklist=['すべて','作業中','完了'];
+
+
+
 function view(st=0){
   const comment =document.getElementById('comment');
   comment.innerHTML = '';
   let str = '<tr><th>ID</th><th>コメント</th><th>状態</th></tr>';
-  lists_new = lists;
-  if(st !=0){
-    lists_new =lists.filter(val=>val.status==st);
-  }
-  if(lists_new.length > 0){
-    lists_new.forEach((val, key) => {
-      str +=  '<tr><td>'+key+'</td><td>'+val.comment+'</td><td><button value='+key+' onclick="status(this)">'+checklist[val.status]  +'</button ></td><td><button value='+key+' onclick="del(this)">削除</button></td></tr>';
+  if(lists.length > 0){
+    lists.forEach((val, key) => {
+      let style ='';
+      if( val.status != st && st > 0){
+        style = ' style="display:none"';
+      }
+      str +=  '<tr'+style+'><td>'+key+'</td><td>'+val.comment+'</td><td><button value='+key+' onclick="status(this)">'+checklist[val.status]  +'</button ></td><td><button value='+key+' onclick="del(this)">削除</button></td></tr>';
     })
     comment.innerHTML = str;
   }
 }
+
 function todolist(){
     const task =document.getElementById("task");
     if(task.value !== ''){
@@ -24,6 +28,8 @@ function todolist(){
     task.value="";
     view();
 }
+
+
 const del= (button)=>{
   lists.splice(button.value,1);
   view();
