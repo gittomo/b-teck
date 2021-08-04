1148170904ade7247314adbb929e0d00579528e9
peko-rin <who_am_i@mrj.biglobe.ne.jp>
2021-08-04 13:23:50 +0900

    pushに変更

--- react/01.js
+++ react/01.js
@@ -5,6 +5,7 @@ class  TodoApp extends React.Component {
     this.state={
       todoList: [],
       value: "",
+      state:"作業中",
     }
   }
 
@@ -13,18 +14,20 @@ class  TodoApp extends React.Component {
   }
 
   add() {
+    let arr = this.state.todoList;
+    arr.push(this.state.value);
+
     this.setState({
-      todoList: this.state.todoList.concat(this.state.value),
+      todoList: arr,
       value: "",
     })
   }
-
+  
   render() {
     const todoList =
-      this.state.todoList.map((todo, idx) => {
-      return <tr key={idx}><td>{idx} </td>{todo}<td><button> 作業中 </button></td><td> <button> 削除 </button></td> </tr>
+    this.state.todoList.map((todo, idx) => {
+      return <tr key={idx}><td>{idx}</td>{todo}<td><button>作業中</button></td><td><button> 削除 </button></td></tr>
     })
-
     return (
       <div>
         <h2>ToDoリスト</h2>
@@ -34,10 +37,10 @@ class  TodoApp extends React.Component {
             <input type="radio" name="check" value="1"/>作業中
             <input type="radio" name="check" value="2"/>完了
 
-        <table>
+        <table><tbody>
         <tr><th>ID</th><th>コメント</th><th>状態</th></tr>
-          {todoList}
-        </table>
+        {todoList}
+        </tbody></table>
         <h2>新規タスクの追加</h2>
           </form>
             <input type="text" value={this.state.value} onChange={e => this.onChange(e)}/>
